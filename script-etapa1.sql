68f81deb8714091260e28c8a536ac2f1fac8fda1
Ghaleon <alannaidon@yahoo.com.br>
2016-06-15 23:56:49 -0300

    First Commit

---
+++ script-etapa1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS dominios;
+USE dominios;
+
+CREATE TABLE IF NOT EXISTS `tabelao` (
+  `idtabelao` INT NOT NULL AUTO_INCREMENT,
+  `dominio` VARCHAR(100) NOT NULL,
+  `documento` VARCHAR(30) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `cep` VARCHAR(9) NOT NULL,
+  `data_cadastro` VARCHAR(20) NOT NULL,
+  `ultima_atualizacao` VARCHAR(20) NOT NULL,
+  `ticket` INT NOT NULL,
+  PRIMARY KEY (`idtabelao`))
+ENGINE = InnoDB;

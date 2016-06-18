28168fcccbcec94de541a16f37a6d41d71903778
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2016-06-18 17:45:58 -0300

    Script para carga das tabelas normalizadas adicionado

---
+++ cargaNormalizada.sql
@@ -0,0 +1,13 @@
+INSERT INTO localidade (uf, cidade, cep)
+SELECT distinct uf, cidade, cep
+FROM tabelao;
+
+INSERT INTO instituicao (documento, nome, id_localidade)
+SELECT DISTINCT tabelao.documento, tabelao.nome, localidade.id_localidade
+FROM tabelao, localidade
+WHERE tabelao.uf=localidade.uf AND tabelao.cidade=localidade.cidade AND tabelao.cep=localidade.cep;
+
+INSERT INTO dominio (dominio, data_cadastro, ultima_atualizacao, ticket, id_instituicao)
+SELECT tabelao.dominio, tabelao.data_cadastro, tabelao.ultima_atualizacao, tabelao.ticket, instituicao.id_instituicao
+FROM tabelao, instituicao
+WHERE tabelao.documento=instituicao.documento AND tabelao.nome=instituicao.nome
